ca033b7d061367dd6cac63ceb5694a68abf9a5f1
TKNW <kyon1.048596@gmail.com>
2020-09-13 21:41:23 +0800

    0913working

--- Assets/RemptyTool/MessageSystem/Scripts/UsageCase.cs
+++ Assets/RemptyTool/MessageSystem/Scripts/UsageCase.cs
@@ -9,27 +9,39 @@ using System.Diagnostics.Tracing;
 public class UsageCase : MonoBehaviour
 {
     private ES_MessageSystem msgSys;
+    [Header("For talking")]
     public Text uiText;
     public Image CCG;
     public TextAsset textAsset;
     public Text Name;
+    [Header("For selecting")]
+    public Text Answer1;
+    public Text Answer2;
+
     private List<string> textList = new List<string>();
     private int textIndex = 0;
     private GameObject Panel;
+    private GameObject Select;
     private bool Istalking = false;
 
+    private bool Isselecting = false;
+    private bool Waitforselect = false;
     void Start()
     {
         Panel = GameObject.Find("TalkCanvas");
+        Select = GameObject.Find("SelectCanvas");
         Panel.SetActive(false);
+        Select.SetActive(false);
+        CCG.color = new Color(255, 255, 255, 0);
+        Name.color = new Color(0, 0, 0, 0);
         msgSys = this.GetComponent<ES_MessageSystem>();
         if (uiText == null)
         {
             Debug.LogError("UIText Component not assign.");
         }
-
         //add special chars and functions in other component.
         msgSys.AddSpecialCharToFuncMap("Close", Closedialog);
+        msgSys.AddSpecialCharToFuncMap("Select", AwakeSelect);
         #region CCG
         msgSys.AddSpecialCharToFuncMap("M1", M1);
         msgSys.AddSpecialCharToFuncMap("M2", M2);
@@ -44,6 +56,17 @@ public class UsageCase : MonoBehaviour
         CCG.color = new Color(255,255,255,0);
         Name.color = new Color(0,0,0,0);
         Istalking = false;
+    }
+    private void AwakeSelect()
+    {
+        Isselecting = true;
+        uiText.text = textList[textIndex];
+        textIndex++;
+        Answer1.text = textList[textIndex];
+        textIndex++;
+        Answer2.text = textList[textIndex];
+        textIndex++;
+        Select.SetActive(true);
     }
     #region SpicF
     private void M1()
@@ -118,13 +141,12 @@ public class UsageCase : MonoBehaviour
     void Update()
     {
 
-        if (Istalking)
+        if (Istalking && !Isselecting)
         {
             if (Input.GetKeyDown(KeyCode.Q))
             {
                 RunText();
             }
-
             //If the message is complete, stop updating text.
             if (msgSys.IsCompleted == false)
             {
